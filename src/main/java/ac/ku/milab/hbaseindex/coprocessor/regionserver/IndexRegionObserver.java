dcb271feb026780a2fec5ab076c24bb806624101
sw <jsw161@korea.ac.kr>
2017-03-08 17:40:05 +0900

    uhm....

--- src/main/java/ac/ku/milab/hbaseindex/coprocessor/regionserver/IndexRegionObserver.java
+++ src/main/java/ac/ku/milab/hbaseindex/coprocessor/regionserver/IndexRegionObserver.java
@@ -5,11 +5,14 @@ import java.util.List;
 import java.util.Map;
 import java.util.NavigableSet;
 
+import org.apache.commons.cli.ParseException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.CellUtil;
 import org.apache.hadoop.hbase.HRegionInfo;
+import org.apache.hadoop.hbase.Server;
+import org.apache.hadoop.hbase.ServerName;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Durability;
 import org.apache.hadoop.hbase.client.IsolationLevel;
@@ -23,11 +26,19 @@ import org.apache.hadoop.hbase.regionserver.HRegion;
 import org.apache.hadoop.hbase.regionserver.HStore;
 import org.apache.hadoop.hbase.regionserver.KeyValueScanner;
 import org.apache.hadoop.hbase.regionserver.Region;
+import org.apache.hadoop.hbase.regionserver.Region.Operation;
+import org.apache.hadoop.hbase.regionserver.RegionServerServices;
 import org.apache.hadoop.hbase.regionserver.ScanInfo;
+import org.apache.hadoop.hbase.regionserver.SplitTransactionFactory;
+import org.apache.hadoop.hbase.regionserver.SplitTransactionImpl;
 import org.apache.hadoop.hbase.regionserver.Store;
 import org.apache.hadoop.hbase.regionserver.StoreScanner;
 import org.apache.hadoop.hbase.regionserver.wal.WALEdit;
+import org.apache.hadoop.hbase.security.User;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.hbase.util.PairOfSameType;
+import org.apache.hadoop.hbase.util.RegionSplitter;
+import org.apache.hadoop.hbase.util.RegionSplitter.HexStringSplit;
 
 import ac.ku.milab.hbaseindex.IdxFilter;
 import ac.ku.milab.hbaseindex.util.IdxConstants;
@@ -40,6 +51,7 @@ public class IndexRegionObserver extends BaseRegionObserver {
 	//private IdxManager indexManager = IdxManager.getInstance();
 
 	private static final byte[] COLUMN_FAMILY = Bytes.toBytes("cf1");
+	private static int cnt = 0;
 
 //	@Override
 //	public void stop(CoprocessorEnvironment e) throws IOException {
@@ -152,17 +164,79 @@ public class IndexRegionObserver extends BaseRegionObserver {
 		boolean isUserTable = TableUtils.isUserTable(Bytes.toBytes(sTableName));
 		
 		if(isUserTable){
+			RegionServerServices rsService = ctx.getEnvironment().getRegionServerServices();
+			ServerName serverName = rsService.getServerName();
+			
 			byte[] leftRegionKey = l.getRegionInfo().getStartKey();
 			byte[] rightRegionKey = r.getRegionInfo().getStartKey();
+			LOG.info("left key is" + leftRegionKey);
+			LOG.info("right key is" + rightRegionKey);
 			
 			String sIdxTableName = TableUtils.getIndexTableName(sTableName);
 			TableName idxTableName = TableName.valueOf(sIdxTableName);
 			
 			List<Region> idxRegionList = ctx.getEnvironment().getRegionServerServices().getOnlineRegions(idxTableName);
+			
+//			for(Region idxRegion : idxRegionList){
+//				byte[] splitKey = Bytes.add(Bytes.toBytes("22거1234"), Bytes.toBytes(14568879l));
+//				
+//				r.startRegionOperation();
+//				LOG.info("Split Start");
+//				String arg = "bin/hbase org.apache.hadoop.hbase.util.RegionSplitter -r -o 2 myTable UniformSplit";
+//				String[] args = new String[]{"bin/hbase org.apache.hadoop.hbase.util.RegionSplitter", "-r", "-o", "2", "myTable", "UniformSplit"};
+//				try {
+//					RegionSplitter.main(args);
+//					LOG.info("Split Complete");
+//				} catch (InterruptedException e) {
+//					// TODO Auto-generated catch block
+//					LOG.info("Split Fail");
+//					e.printStackTrace();
+//				} catch (ParseException e) {
+//					// TODO Auto-generated catch block
+//					LOG.info("Split Fail");
+//					e.printStackTrace();
+//				}
+//				
+//				r.closeRegionOperation();
+			
 			for(Region idxRegion : idxRegionList){
 				byte[] regionKey = idxRegion.getRegionInfo().getStartKey();
 				if(Bytes.contains(regionKey, leftRegionKey)){
+					//HexStringSplit splitter = new RegionSplitter.HexStringSplit();
+					//splitter.split(leftRegionKey, rightRegionKey);
+					SplitTransactionImpl splitter = new SplitTransactionImpl(idxRegion, rightRegionKey);
+					if(splitter.prepare()){
+						try{
+							splitter.execute((Server)rsService, rsService);
+							LOG.info("Split Complete");
+						}catch(Exception e){
+							e.printStackTrace();
+						}
+					}
 					
+//					byte[] splitKey = Bytes.add(Bytes.toBytes("22거1234"), Bytes.toBytes(14568879l));
+////					HexStringSplit split = new RegionSplitter.HexStringSplit();
+////					split.split(2);
+//					//r.closeRegionOperation();
+//					r.startRegionOperation();
+//					LOG.info("Split Start");
+//					String arg = "bin/hbase org.apache.hadoop.hbase.util.RegionSplitter -r -o 2 myTable UniformSplit";
+//					String[] args = new String[]{"bin/hbase org.apache.hadoop.hbase.util.RegionSplitter", "-r", "-o", "2", "myTable", "UniformSplit"};
+//					try {
+//						RegionSplitter.main(args);
+//						LOG.info("Split Complete");
+//					} catch (InterruptedException e) {
+//						// TODO Auto-generated catch block
+//						LOG.info("Split Fail");
+//						e.printStackTrace();
+//					} catch (ParseException e) {
+//						// TODO Auto-generated catch block
+//						LOG.info("Split Fail");
+//						e.printStackTrace();
+//					}
+//					
+//					r.closeRegionOperation();
+//					
 				}
 			}
 		}
@@ -251,7 +325,7 @@ public class IndexRegionObserver extends BaseRegionObserver {
 			double dLat = Bytes.toDouble(lat);
 			double dLon = Bytes.toDouble(lon);
 			
-			LOG.info("prePut processing - " + sCarNum + ","+lTime+","+dLat+","+dLon);
+			//LOG.info("prePut processing - " + sCarNum + ","+lTime+","+dLat+","+dLon);
 
 			// get index column
 //			List<IdxColumnQualifier> idxColumns = indexManager.getIndexOfTable(tableName);
@@ -290,12 +364,106 @@ public class IndexRegionObserver extends BaseRegionObserver {
 
 			// index table and put
 			List<Region> idxRegions = ctx.getEnvironment().getRegionServerServices().getOnlineRegions(idxTName);
-			Region idxRegion = idxRegions.get(0);
-			idxRegion.put(idxPut);
+			int size = idxRegions.size();
+			for(int i=0;i<size;i++){
+				Region r = idxRegions.get(i);
+				byte[] idxStartKey = r.getRegionInfo().getStartKey();
+				int compareRes = Bytes.compareTo(indexRowKey, idxStartKey);
+				if(compareRes!=-1){
+					if(i==size-1||compareRes==0){
+						//LOG.info("index OK");
+						r.put(idxPut);
+					}else{
+						Region nextRegion = idxRegions.get(i+1);
+						byte[] idxNextKey = nextRegion.getRegionInfo().getStartKey();
+						if(Bytes.compareTo(indexRowKey, idxNextKey)==-1){
+							//LOG.info("index OK");
+							r.put(idxPut);
+						}
+					}
+				}
+			}
+			//Region idxRegion = idxRegions.get(0);
+			//idxRegion.put(idxPut);
+			
+//			cnt++;
+//			if(cnt==100){
+//				Region r = ctx.getEnvironment().getRegion();
+//				r.startRegionOperation(Operation.SPLIT_REGION);
+//				byte[] splitKey = Bytes.add(Bytes.toBytes("22거1234"), Bytes.toBytes(14568879l));
+//				SplitTransactionImpl splitter = new SplitTransactionImpl(r, splitKey);
+//				if(splitter.prepare()){
+//					try{
+//						splitter.execute((Server)ctx.getEnvironment().getRegionServerServices(), ctx.getEnvironment().getRegionServerServices());
+//						LOG.info("Split Complete");
+//					}catch(Exception e){
+//						e.printStackTrace();
+//						LOG.info("Split Fail");
+//					}
+//				}
+//				r.closeRegionOperation();
+//			}
 		}
+		
+		//LOG.info("PrePut END : " + tableName);
 
-		LOG.info("PrePut END : " + tableName);
+	}
+	
+	@Override
+	public void postPut(ObserverContext<RegionCoprocessorEnvironment> ctx, Put put, WALEdit edit, Durability durability)
+			throws IOException {
+		// TODO Auto-generated method stub
+		TableName tName = ctx.getEnvironment().getRegionInfo().getTable();
+		String tableName = tName.getNameAsString();
 
+		// LOG.info("PrePut START : " + tableName);
+
+		// if table is not user table, it is not performed
+		boolean isUserTable = TableUtils.isUserTable(Bytes.toBytes(tableName));
+		if (isUserTable) {
+			cnt++;
+			if(cnt==100){
+				Region r = ctx.getEnvironment().getRegion();
+				//r.startRegionOperation(Operation.SPLIT_REGION);
+//				byte[] splitKey = Bytes.add(Bytes.toBytes("22거1234"), Bytes.toBytes(14568879l));
+////				HexStringSplit split = new RegionSplitter.HexStringSplit();
+////				split.split(2);
+//				//r.closeRegionOperation();
+//				r.startRegionOperation();
+//				LOG.info("Split Start");
+//				String arg = "bin/hbase org.apache.hadoop.hbase.util.RegionSplitter -r -o 2 test UniformSplit";
+//				String[] args = new String[]{"bin/hbase org.apache.hadoop.hbase.util.RegionSplitter", "-r", "-o", "2", "test", "UniformSplit"};
+//				try {
+//					RegionSplitter.main(args);
+//					LOG.info("Split Complete");
+//				} catch (InterruptedException e) {
+//					// TODO Auto-generated catch block
+//					LOG.info("Split Fail");
+//					e.printStackTrace();
+//				} catch (ParseException e) {
+//					// TODO Auto-generated catch block
+//					LOG.info("Split Fail");
+//					e.printStackTrace();
+//				}
+//				
+//				r.closeRegionOperation();
+				
+				
+//				SplitTransactionImpl splitter = new SplitTransactionImpl(r, splitKey);
+//				if(splitter.prepare()){
+//					try{
+//						LOG.info("Split Complete");
+//						User user = User.getCurrent();
+//						PairOfSameType<Region> regions = splitter.execute((Server)ctx.getEnvironment().getRegionServerServices(), ctx.getEnvironment().getRegionServerServices(), user);
+//						regions = splitter.stepsBeforePONR(arg0, arg1, arg2)
+//					}catch(Exception e){
+//						LOG.info("Split Fail");
+//						e.printStackTrace();
+//					}
+//				}
+				//r.closeRegionOperation();
+			}
+		}
 	}
 
 //	@Override
